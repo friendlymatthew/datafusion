b79a5b99df29bb3219668c08472fc029b63d678e
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2025-03-22 10:01:09 -0400

    Format Date32 to string given timestamp specifiers

--- datafusion/functions/src/datetime/to_char.rs
+++ datafusion/functions/src/datetime/to_char.rs
@@ -20,6 +20,7 @@ use std::sync::Arc;
 
 use arrow::array::cast::AsArray;
 use arrow::array::{new_null_array, Array, ArrayRef, StringArray};
+use arrow::compute::cast;
 use arrow::datatypes::DataType;
 use arrow::datatypes::DataType::{
     Date32, Date64, Duration, Time32, Time64, Timestamp, Utf8,
@@ -178,7 +179,9 @@ fn _build_format_options<'a>(
         return Ok(FormatOptions::new());
     };
     let format_options = match data_type {
-        Date32 => FormatOptions::new().with_date_format(Some(format)),
+        Date32 => FormatOptions::new()
+            .with_date_format(Some(format))
+            .with_datetime_format(Some(format)),
         Date64 => FormatOptions::new().with_datetime_format(Some(format)),
         Time32(_) => FormatOptions::new().with_time_format(Some(format)),
         Time64(_) => FormatOptions::new().with_time_format(Some(format)),
@@ -225,7 +228,13 @@ fn _to_char_scalar(
         Err(value) => return value,
     };
 
-    let formatter = ArrayFormatter::try_new(array.as_ref(), &format_options)?;
+    let value = if data_type == &Date32 {
+        &cast(array.as_ref(), &Date64)?
+    } else {
+        &array
+    };
+
+    let formatter = ArrayFormatter::try_new(value, &format_options)?;
     let formatted: Result<Vec<Option<String>>, ArrowError> = (0..array.len())
         .map(|i| {
             if array.is_null(i) {
@@ -271,9 +280,16 @@ fn _to_char_array(args: &[ColumnarValue]) -> Result<ColumnarValue> {
             Ok(value) => value,
             Err(value) => return value,
         };
+
+        let value = if data_type == &Date32 {
+            &cast(arrays[0].as_ref(), &Date64)?
+        } else {
+            &arrays[0]
+        };
+
         // this isn't ideal but this can't use ValueFormatter as it isn't independent
         // from ArrayFormatter
-        let formatter = ArrayFormatter::try_new(arrays[0].as_ref(), &format_options)?;
+        let formatter = ArrayFormatter::try_new(value, &format_options)?;
         let result = formatter.value(idx).try_to_string();
         match result {
             Ok(value) => results.push(Some(value)),
@@ -327,6 +343,11 @@ mod tests {
                 ScalarValue::Date32(Some(18506)),
                 ScalarValue::Utf8(Some("%Y::%m::%d".to_string())),
                 "2020::09::01".to_string(),
+            ),
+            (
+                ScalarValue::Date32(Some(18506)),
+                ScalarValue::Utf8(Some("%Y::%m::%d %S::%M::%H %f".to_string())),
+                "2020::09::01 00::00::00 000000000".to_string(),
             ),
             (
                 ScalarValue::Date64(Some(date.and_utc().timestamp_millis())),
